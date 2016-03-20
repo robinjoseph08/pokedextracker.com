ea668cd51d6d7978fb74c2a6d95b90879039d02f
Ami Wang <ami@lob.com>
2016-03-20 15:38:47 -0700

    feat(dex): boxes base!!!

--- app/components/box.ts
+++ app/components/box.ts
@@ -1,8 +1,11 @@
 import { Component } from 'angular2/core';
 
+import { PokemonComponent } from './pokemon';
+
 const HTML = require('../views/box.html');
 
 @Component({
+  directives: [PokemonComponent],
   selector: 'box',
   template: HTML
 })
---
+++ app/components/pokemon.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+const HTML = require('../views/pokemon.html');
+
+@Component({
+  selector: 'pokemon',
+  template: HTML
+})
+export class PokemonComponent {}
