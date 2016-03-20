55e018651fb1465baaaf96045129196e1ec98fe0
Ami Wang <ami@lob.com>
2016-03-19 19:44:48 -0700

    feat(dex): dex header

--- app/components/dex.ts
+++ app/components/dex.ts
@@ -1,8 +1,12 @@
 import { Component } from 'angular2/core';
 
+import { BoxComponent }    from './box';
+import { HeaderComponent } from './header';
+
 const HTML = require('../views/dex.html');
 
 @Component({
+  directives: [HeaderComponent, BoxComponent],
   selector: 'dex',
   template: HTML
 })
