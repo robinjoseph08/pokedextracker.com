bfafe0c03dc07c7e91a215565ec0a99a782ffe99
Robin Joseph <robin@lob.com>
2016-04-07 21:45:12 -0700

    feat(nav): pass user to nav

--- app/components/nav.ts
+++ app/components/nav.ts
@@ -1,19 +1,26 @@
 import { Component }  from 'angular2/core';
 import { RouterLink } from 'angular2/router';
 
-import { User } from '../classes/user';
+import { SessionService } from '../services/session';
+import { User }           from '../classes/user';
 
 const HTML = require('../views/nav.html');
 
 @Component({
   directives: [RouterLink],
   inputs: ['user'],
+  providers: [SessionService],
   selector: 'nav',
   template: HTML
 })
 export class NavComponent {
 
+  public _session: SessionService;
   public user: User;
+
+  constructor (_session: SessionService) {
+    this._session = _session;
+  }
 
   public signOut () {
     localStorage.removeItem('token');
