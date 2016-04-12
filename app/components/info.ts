75e63f3afc01de9c9e089f728d53960ebab8ccd4
Ami Wang <ami@lob.com>
2016-04-11 21:38:22 -0700

    fix(tracker): loading text

--- app/components/info.ts
+++ app/components/info.ts
@@ -16,6 +16,8 @@ const HTML = require('../views/info.html');
 })
 export class InfoComponent {
 
+  public loading: boolean = true;
+
   public collapsedChange = new EventEmitter<boolean>();
 
   public get active (): Capture {
@@ -23,10 +25,14 @@ export class InfoComponent {
   }
 
   public set active (active: Capture) {
+    this.loading = true;
+    console.log('beginning: ' + this.loading);
     this._active = active;
     if (!active.pokemon.bulbapedia_url) {
       active.loadPokemon();
     }
+    this.loading = false;
+    console.log('end: ' + this.loading);
   }
 
   private _active: Capture;
