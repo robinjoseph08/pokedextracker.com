c945f484463285e0f639efcc00f0577d03f1aa23
Robin Joseph <robin@lob.com>
2016-04-13 01:23:02 -0700

    feat(tracker): close things when clicking outside

--- app/components/header.ts
+++ app/components/header.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { PercentPipe }             from 'angular2/common';
 
-import { Capture }        from '../classes/capture';
-import { SessionService } from '../services/session';
-import { User }           from '../classes/user';
+import { Capture }           from '../classes/capture';
+import { OffClickDirective } from '../directives/off-click';
+import { SessionService }    from '../services/session';
+import { User }              from '../classes/user';
 
 const HTML = require('../views/header.html');
 
 @Component({
+  directives: [OffClickDirective],
   events: ['regionChange'],
   inputs: ['captures', 'region', 'user'],
   pipes: [PercentPipe],
@@ -22,12 +24,16 @@ export class HeaderComponent {
   public region: string;
   public regions: string[] = ['national', 'kanto', 'johto', 'hoenn', 'sinnoh', 'unova', 'kalos'];
   public _session: SessionService;
+  public showLink: boolean = false;
   public user: User;
 
   public regionChange = new EventEmitter<string>();
 
   constructor (_session: SessionService) {
     this._session = _session;
+
+    this.closeRegion = this.closeRegion.bind(this);
+    this.closeShare = this.closeShare.bind(this);
   }
 
   public get caught () {
@@ -36,6 +42,14 @@ export class HeaderComponent {
 
   public get total () {
     return this.captures.filter((capture) => capture.pokemon.is(this.region)).length;
+  }
+
+  public closeRegion () {
+    this.dropdown = false;
+  }
+
+  public closeShare () {
+    this.showLink = false;
   }
 
 }
