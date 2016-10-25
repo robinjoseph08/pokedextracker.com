b5f49c651464f85713bd2877d80e3f736c641ff0
Robin Joseph <robin@lob.com>
2016-10-24 22:37:58 -0700

    fix(load): wait for DOMContentLoaded if necessary

--- app/index.jsx
+++ app/index.jsx
@@ -6,9 +6,17 @@ import { render }   from 'react-dom';
 import { AppComponent } from './components/app';
 import { Store }        from './stores';
 
-render(
-  <Provider store={Store}>
-    <AppComponent />
-  </Provider>,
-  document.getElementById('root')
-);
+function run () {
+  render(
+    <Provider store={Store}>
+      <AppComponent />
+    </Provider>,
+    document.getElementById('root')
+  );
+}
+
+if (document.getElementById('root')) {
+  run();
+} else {
+  window.addEventListener('DOMContentLoaded', run);
+}
