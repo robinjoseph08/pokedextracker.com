3348944c657664642cfd2e2e6625e71f8c262d94
Simen Bekkhus <sbekkhus91@gmail.com>
2017-01-28 15:42:39 +0100

    chore(devtools): Add hook for redux devtools

--- app/stores/index.js
+++ app/stores/index.js
@@ -1,7 +1,7 @@
-import { browserHistory }               from 'react-router';
-import { createStore, applyMiddleware } from 'redux';
-import { routerMiddleware }             from 'react-router-redux';
-import thunkMiddleware                  from 'redux-thunk';
+import { browserHistory }                        from 'react-router';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware }                      from 'react-router-redux';
+import thunkMiddleware                           from 'redux-thunk';
 
 import { loadState, saveState } from '../utils/local-storage';
 import { reducer }              from '../reducers';
@@ -9,6 +9,8 @@ import { reducer }              from '../reducers';
 const initialState = loadState();
 const router = routerMiddleware(browserHistory);
 
-export const Store = createStore(reducer, initialState, applyMiddleware(thunkMiddleware, router));
+const composeEnhancers = process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const Store = createStore(reducer, initialState, composeEnhancers(applyMiddleware(thunkMiddleware, router)));
 
 Store.subscribe(() => saveState(Store.getState()));
