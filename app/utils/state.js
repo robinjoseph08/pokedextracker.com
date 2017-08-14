2afda0498e9886aad817570dce2880869e1f57ac
Robin Joseph <robin@lob.com>
2017-08-13 21:31:35 -0700

    fix(local-storage): make site usable without local storage

---
+++ app/utils/state.js
@@ -0,0 +1,35 @@
+import { Raven }        from '../utils/analytics';
+import { localStorage } from '../utils/local-storage';
+
+export function tokenToUser (token) {
+  if (!token) {
+    Raven.setUserContext();
+    return null;
+  }
+
+  const user = JSON.parse(atob(token.split('.')[1]));
+
+  Raven.setUserContext({ id: user.id, username: user.username });
+
+  return user;
+}
+
+export function loadState () {
+  const notif20170204 = localStorage.getItem('notif-2017.02.04') === 'true' || undefined;
+  const token = localStorage.getItem('token');
+  const session = tokenToUser(token);
+  const showInfo = localStorage.getItem('showInfo') === 'true' || undefined;
+
+  return { notification: notif20170204, token, session, showInfo };
+}
+
+export function saveState ({ notification, showInfo, token }) {
+  if (token) {
+    localStorage.setItem('token', token);
+  } else {
+    localStorage.removeItem('token');
+  }
+
+  localStorage.setItem('notif-2017.02.04', notification);
+  localStorage.setItem('showInfo', showInfo);
+}
