687995f3848f8b3d91c07bbf2b12c9e3bf29513c
Robin Joseph <robinmjoseph08@gmail.com>
2018-01-07 23:36:07 -0800

    feat(docker): add dockerfile and nginx.conf (#292)

--- config/index.js
+++ config/index.js
@@ -3,11 +3,18 @@ import { Local }       from './local';
 import { Production }  from './production';
 import { Staging }     from './staging';
 
-const config = {
+const CONFIG = {
   development: Development,
   local: Local,
   production: Production,
   staging: Staging
 };
 
-export const Config = config[process.env.NODE_ENV];
+const ENVIRONMENTS = {
+  'pokedextracker.com': 'production',
+  'staging.pokedextracker.com': 'staging'
+};
+
+const environment = process.env.NODE_ENV || ENVIRONMENTS[window.location.hostname] || 'development';
+
+export const Config = CONFIG[environment];
