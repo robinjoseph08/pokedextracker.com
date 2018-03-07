9f72375ff111625a1bea7cecf5ae5b2b35d1c738
Ami Wang <ami@lob.com>
2018-03-06 21:48:35 -0800

    add empty state

--- app/components/search-results.jsx
+++ app/components/search-results.jsx
@@ -1,19 +1,45 @@
-import { connect } from 'react-redux';
+import { Component } from 'react';
+import { connect }   from 'react-redux';
 
 import { PokemonComponent } from './pokemon';
+import { setQuery }         from '../actions/search';
 
-export function SearchResults ({ captures, query }) {
-  const filteredCaptures = captures.filter((capture) => capture.pokemon.name.toLowerCase().indexOf(query.toLowerCase()) === 0);
+export class SearchResults extends Component {
 
-  return (
-    <div className="search-results">
-      {filteredCaptures.map((capture) => <PokemonComponent key={capture.pokemon.id} capture={capture} />)}
-    </div>
-  );
+  onClick = () => {
+    this.props.setQuery('');
+  }
+
+  render () {
+    const { captures, query } = this.props;
+
+    const filteredCaptures = captures.filter((capture) => capture.pokemon.name.toLowerCase().indexOf(query.toLowerCase()) === 0);
+
+    if (filteredCaptures.length === 0) {
+      return (
+        <div className="search-results">
+          <p>No results. <a className="link" onClick={this.onClick}>Clear your search?</a></p>
+        </div>
+      );
+    }
+
+    return (
+      <div className="search-results">
+        {filteredCaptures.map((capture) => <PokemonComponent key={capture.pokemon.id} capture={capture} />)}
+      </div>
+    );
+  }
+
 }
 
 function mapStateToProps ({ query }) {
   return { query };
 }
 
-export const SearchResultsComponent = connect(mapStateToProps)(SearchResults);
+function mapDispatchToProps (dispatch) {
+  return {
+    setQuery: (query) => dispatch(setQuery(query))
+  };
+}
+
+export const SearchResultsComponent = connect(mapStateToProps, mapDispatchToProps)(SearchResults);
