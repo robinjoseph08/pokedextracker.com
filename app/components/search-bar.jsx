9f124bc1bb894398c6d6fbe3315f17091a17a23f
Robin Joseph <robin@lob.com>
2018-05-05 16:12:59 -0500

    add / keyboard shortcut

--- app/components/search-bar.jsx
+++ app/components/search-bar.jsx
@@ -1,12 +1,25 @@
 import { Component } from 'react';
 import { connect }   from 'react-redux';
 
+import { ReactGA }  from '../utils/analytics';
 import { setQuery } from '../actions/search';
 
 export class SearchBar extends Component {
 
   componentWillMount () {
+    document.addEventListener('keyup', this.onKeyup);
     this.props.setQuery('');
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keyup', this.onKeyup);
+  }
+
+  onKeyup = (event) => {
+    if (event.target.tagName.toLowerCase() !== 'input' && event.key === '/') {
+      ReactGA.event({ action: 'used shortcut', category: 'Search' });
+      this._search && this._search.focus();
+    }
   }
 
   onClick = () => {
@@ -26,7 +39,7 @@ export class SearchBar extends Component {
         <div className="wrapper">
           <div className="form-group">
             <i className="fa fa-search" />
-            <input className="form-control" ref={(c) => this._search = c} name="search" id="search" type="text" placeholder="Search by Pokémon name..." autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" onChange={(e) => setQuery(e.target.value)} />
+            <input className="form-control" ref={(c) => this._search = c} name="search" id="search" type="text" placeholder="Search by Pokémon name (press / to focus)" autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" onChange={(e) => setQuery(e.target.value)} />
             {query.length > 0 ? <a onClick={this.onClick}><i className="fa fa-times" /></a> : null}
           </div>
         </div>
