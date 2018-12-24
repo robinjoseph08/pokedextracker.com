c1baa5749a69b5749ace883ffa85ac384c7f61f1
Robin Joseph <robinmjoseph08@gmail.com>
2018-12-24 11:21:31 -0600

    feat(locations): use pokemon.locations to simplify logic (#312)

--- app/components/info.jsx
+++ app/components/info.jsx
@@ -64,7 +64,7 @@ export class Info extends Component {
             <h2>#{padding(pokemon.national_id, 3)}</h2>
           </div>
 
-          <InfoLocationsComponent gameFamily={dex.game.game_family} pokemon={pokemon} regional={dex.regional} />
+          <InfoLocationsComponent pokemon={pokemon} />
 
           <EvolutionFamilyComponent family={pokemon.evolution_family} />
 
