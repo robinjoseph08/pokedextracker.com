371c7e54ca3f415567004307f9b1780c5109ca40
Robin Joseph <robin@lob.com>
2019-01-04 20:36:34 +0900

    modify switch fc formatting

--- app/utils/formatting.js
+++ app/utils/formatting.js
@@ -25,7 +25,23 @@ export function friendCode3DS (code) {
 }
 
 export function friendCodeSwitch (code) {
-  return code && code.replace(FRIEND_CODE_SWITCH_REGEX, '$1-');
+  if (!code) {
+    return code;
+  }
+
+  code = code.toUpperCase();
+
+  // Allow the user to type in `SW-`, but if they start typing something else
+  // in, force `SW-` in front of whatever it was that they were typing. This
+  // will make it so that if they follow the formatting rules, nothing will
+  // self-correct, but if they don't, it will.
+  ['S', 'W', '-'].forEach((letter, i) => {
+    if (code[i] && code[i] !== letter) {
+      code = code.slice(0, i) + letter + code.slice(i);
+    }
+  });
+
+  return code.replace(FRIEND_CODE_SWITCH_REGEX, '$1-');
 }
 
 export function padding (number, digits, value = '0') {
