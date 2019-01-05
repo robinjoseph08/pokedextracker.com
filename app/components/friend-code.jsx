34dbc27b4d09b278bd790869d9ac53112d8b8f1f
Ami Wang <ami@lob.com>
2019-01-05 10:03:20 -0800

    replace 0s with Xs

--- app/components/friend-code.jsx
+++ app/components/friend-code.jsx
@@ -8,7 +8,7 @@ export function FriendCode ({ session, user }) {
 
   return (
     <h2>
-      <b>3DS FC</b>: <span className={user.friend_code_3ds ? '' : 'fc-missing'}>{user.friend_code_3ds || '0000-0000-0000'}</span>, <b>Switch FC</b>: <span className={user.friend_code_switch ? '' : 'fc-missing'}>{user.friend_code_switch || 'SW-0000-0000-0000'}</span>
+      <b>3DS FC</b>: <span className={user.friend_code_3ds ? '' : 'fc-missing'}>{user.friend_code_3ds || 'XXXX-XXXX-XXXX'}</span>, <b>Switch FC</b>: <span className={user.friend_code_switch ? '' : 'fc-missing'}>{user.friend_code_switch || 'SW-XXXX-XXXX-XXXX'}</span>
       {ownPage ? <Link to="/account" onClick={() => ReactGA.event({ action: 'click edit friend code', category: 'User' })}><i className="fa fa-pencil" /></Link> : null}
     </h2>
   );
