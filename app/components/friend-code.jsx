713abafcb7b7edde7c9bb35f091ddc5d287e73c4
Ami Wang <ami@lob.com>
2019-01-09 21:35:07 -0800

    stack friend codes

--- app/components/friend-code.jsx
+++ app/components/friend-code.jsx
@@ -1,16 +1,28 @@
-import { Link }    from 'react-router';
-import { connect } from 'react-redux';
+import { Link }     from 'react-router';
+import { connect }  from 'react-redux';
 
 import { ReactGA } from '../utils/analytics';
 
 export function FriendCode ({ session, user }) {
   const ownPage = session && session.id === user.id;
 
+  let editAccountBtn = null;
+
+  if (ownPage) {
+    editAccountBtn = (
+      <Link to="/account" onClick={() => ReactGA.event({ action: 'click edit friend code', category: 'User' })}><i className="fa fa-pencil" /></Link>
+    );
+  }
+
   return (
-    <h2>
-      <b>3DS FC</b>: <span className={user.friend_code_3ds ? '' : 'fc-missing'}>{user.friend_code_3ds || 'XXXX-XXXX-XXXX'}</span>, <b>Switch FC</b>: <span className={user.friend_code_switch ? '' : 'fc-missing'}>{user.friend_code_switch || 'SW-XXXX-XXXX-XXXX'}</span>
-      {ownPage ? <Link to="/account" onClick={() => ReactGA.event({ action: 'click edit friend code', category: 'User' })}><i className="fa fa-pencil" /></Link> : null}
-    </h2>
+    <div>
+      <h2>
+        <b>3DS FC</b>: <span className={user.friend_code_3ds ? '' : 'fc-missing'}>{user.friend_code_3ds || 'XXXX-XXXX-XXXX'}</span> {editAccountBtn}
+      </h2>
+      <h2>
+        <b>Switch FC</b>: <span className={user.friend_code_switch ? '' : 'fc-missing'}>{user.friend_code_switch || 'SW-XXXX-XXXX-XXXX'}</span> {editAccountBtn}
+      </h2>
+    </div>
   );
 }
 
