9a04fb8902ef4c78d40f268a05787d9747b49005
Robin Joseph <robin@lob.com>
2019-01-12 20:23:55 -0800

    fix spacing

--- app/components/friend-code.jsx
+++ app/components/friend-code.jsx
@@ -1,5 +1,5 @@
-import { Link }     from 'react-router';
-import { connect }  from 'react-redux';
+import { Link }    from 'react-router';
+import { connect } from 'react-redux';
 
 import { ReactGA } from '../utils/analytics';
 
