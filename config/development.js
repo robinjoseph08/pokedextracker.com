0df1ac85ff074235f81f8248276233a153ee8307
Robin Joseph <robin@lob.com>
2019-04-13 15:38:25 -0700

    feat(kube): add kubernetes values.yaml files

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
 export const Development = {
-  API_HOST: 'https://staging.api.pokedextracker.com',
+  API_HOST: 'https://api.staging.pokedextracker.com',
   ENVIRONMENT: 'development',
   GA_ID: 'UA-45307701-5',
   SENTRY_DSN: 'https://f2362849081c4b2494b6084fec02300e@sentry.io/108494',
