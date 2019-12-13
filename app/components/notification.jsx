4bb9b19003f492df280de6a3972a03f51ed2c1f7
Ami Wang <ami@lob.com>
2019-12-12 22:47:29 -0800

    typo

--- app/components/notification.jsx
+++ app/components/notification.jsx
@@ -10,7 +10,7 @@ export function Notification ({ notification, setNotification }) {
   return (
     <div className="alert alert-muted">
       <i className="fa fa-times" onClick={() => setNotification(true)} />
-      <p>Sword and Sheild support is here! <a href="http://bit.ly/pt-swsh" target="_blank" rel="noopener noreferrer">Read more</a>.</p>
+      <p>Sword and Shield support is here! <a href="http://bit.ly/pt-swsh" target="_blank" rel="noopener noreferrer">Read more</a>.</p>
     </div>
   );
 }
