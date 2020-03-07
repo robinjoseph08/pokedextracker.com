a4b79336bf6e13e485346407570cec852d2298ff
Robin Joseph <robinmjoseph08@gmail.com>
2020-03-06 18:27:45 -0800

    update info component

--- app/components/info.jsx
+++ app/components/info.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import { connect }   from 'react-redux';
+import { useEffect }                from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { EvolutionFamilyComponent } from './evolution-family';
 import { InfoLocationsComponent }   from './info-locations';
@@ -9,91 +9,83 @@ import { padding }                  from '../utils/formatting';
 import { retrievePokemon }          from '../actions/pokemon';
 import { setShowInfo }              from '../actions/tracker';
 
-export class Info extends Component {
+export function InfoComponent () {
+  const dispatch = useDispatch();
 
-  componentDidMount () {
-    this.reset();
-  }
+  const currentPokemon = useSelector(({ currentPokemon }) => currentPokemon);
+  const dex = useSelector(({ currentDex, currentUser, users }) => users[currentUser].dexesBySlug[currentDex]);
+  const pokemon = useSelector(({ currentPokemon, pokemon }) => pokemon[currentPokemon]);
+  const showInfo = useSelector(({ showInfo }) => showInfo);
 
-  componentDidUpdate () {
-    this.reset();
-  }
-
-  reset () {
-    const { currentPokemon, dex, pokemon, retrievePokemon } = this.props;
-
+  useEffect(() => {
     if (!pokemon) {
-      retrievePokemon(currentPokemon, { game_family: dex.game.game_family.id, regional: dex.regional });
+      dispatch(retrievePokemon(currentPokemon, {
+        game_family: dex.game.game_family.id,
+        regional: dex.regional
+      }));
     }
+  }, [currentPokemon, dex, pokemon]);
+
+  const handleInfoClick = () => {
+    ReactGA.event({ action: showInfo ? 'collapse' : 'uncollapse', category: 'Info' });
+    dispatch(setShowInfo(!showInfo));
+  };
+
+  let serebiiPath = 'pokedex-swsh';
+  if (dex.game.game_family.generation === 6) {
+    serebiiPath = 'pokedex-xy';
+  } else if (dex.game.game_family.generation === 7) {
+    serebiiPath = 'pokedex-sm';
   }
 
-  setShowInfo = (showInfo) => {
-    const { setShowInfo } = this.props;
-
-    ReactGA.event({ action: showInfo ? 'uncollapse' : 'collapse', category: 'Info' });
-
-    setShowInfo(showInfo);
-  }
-
-  render () {
-    const { dex, pokemon, showInfo } = this.props;
-
-    let serebiiPath = 'pokedex-swsh';
-    if (dex.game.game_family.generation === 6) {
-      serebiiPath = 'pokedex-xy';
-    } else if (dex.game.game_family.generation === 7) {
-      serebiiPath = 'pokedex-sm';
-    }
-
-    if (!pokemon) {
-      return (
-        <div className={`info ${showInfo ? '' : 'collapsed'}`}>
-          <div className="info-collapse" onClick={() => this.setShowInfo(!showInfo)}>
-            <i className={`fa ${showInfo ? 'fa-caret-right' : 'fa-caret-left'}`} />
-          </div>
-
-          <div className="info-main" />
-        </div>
-      );
-    }
-
+  if (!pokemon) {
     return (
       <div className={`info ${showInfo ? '' : 'collapsed'}`}>
-        <div className="info-collapse" onClick={() => this.setShowInfo(!showInfo)}>
+        <div className="info-collapse" onClick={handleInfoClick}>
           <i className={`fa ${showInfo ? 'fa-caret-right' : 'fa-caret-left'}`} />
         </div>
 
-        <div className="info-main">
-          <div className="info-header">
-            <i className={iconClass(pokemon, dex)} />
-            <h1 dangerouslySetInnerHTML={htmlName(pokemon.name)} />
-            <h2>#{padding(pokemon.national_id, 3)}</h2>
-          </div>
-
-          <InfoLocationsComponent pokemon={pokemon} />
-
-          <EvolutionFamilyComponent family={pokemon.evolution_family} />
-
-          <div className="info-footer">
-            <a href={`http://bulbapedia.bulbagarden.net/wiki/${encodeURIComponent(pokemon.name)}_(Pok%C3%A9mon)`} target="_blank" rel="noopener noreferrer" onClick={() => ReactGA.event({ action: 'open Bulbapedia link', category: 'Info', label: pokemon.name })}>Bulbapedia <i className="fa fa-long-arrow-right" /></a>
-            <a href={`http://www.serebii.net/${serebiiPath}/${padding(pokemon.national_id, 3)}.shtml`} target="_blank" rel="noopener noreferrer" onClick={() => ReactGA.event({ action: 'open Serebii link', category: 'Info', label: pokemon.name })}>Serebii <i className="fa fa-long-arrow-right" /></a>
-          </div>
-        </div>
+        <div className="info-main" />
       </div>
     );
   }
 
-}
+  return (
+    <div className={`info ${showInfo ? '' : 'collapsed'}`}>
+      <div className="info-collapse" onClick={handleInfoClick}>
+        <i className={`fa ${showInfo ? 'fa-caret-right' : 'fa-caret-left'}`} />
+      </div>
 
-function mapStateToProps ({ currentDex, currentUser, currentPokemon, pokemon, showInfo, users }) {
-  return { currentPokemon, dex: users[currentUser].dexesBySlug[currentDex], pokemon: pokemon[currentPokemon], showInfo };
-}
+      <div className="info-main">
+        <div className="info-header">
+          <i className={iconClass(pokemon, dex)} />
+          <h1 dangerouslySetInnerHTML={htmlName(pokemon.name)} />
+          <h2>#{padding(pokemon.national_id, 3)}</h2>
+        </div>
 
-function mapDispatchToProps (dispatch) {
-  return {
-    retrievePokemon: (id, query) => dispatch(retrievePokemon(id, query)),
-    setShowInfo: (show) => dispatch(setShowInfo(show))
-  };
-}
+        <InfoLocationsComponent pokemon={pokemon} />
 
-export const InfoComponent = connect(mapStateToProps, mapDispatchToProps)(Info);
+        <EvolutionFamilyComponent family={pokemon.evolution_family} />
+
+        <div className="info-footer">
+          <a
+            href={`http://bulbapedia.bulbagarden.net/wiki/${encodeURIComponent(pokemon.name)}_(Pok%C3%A9mon)`}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => ReactGA.event({ action: 'open Bulbapedia link', category: 'Info', label: pokemon.name })}
+          >
+            Bulbapedia <i className="fa fa-long-arrow-right" />
+          </a>
+          <a
+            href={`http://www.serebii.net/${serebiiPath}/${padding(pokemon.national_id, 3)}.shtml`}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => ReactGA.event({ action: 'open Serebii link', category: 'Info', label: pokemon.name })}
+          >
+            Serebii <i className="fa fa-long-arrow-right" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
