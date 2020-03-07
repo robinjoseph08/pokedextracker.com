5845a9156d3a9309120f9d315612e1e4d21502b0
Robin Joseph <robinmjoseph08@gmail.com>
2020-03-06 21:34:05 -0800

    update dex create component

--- app/components/dex-create.jsx
+++ app/components/dex-create.jsx
@@ -1,163 +1,178 @@
-import { Component } from 'react';
-import Modal         from 'react-modal';
-import { connect }   from 'react-redux';
-import slug          from 'slug';
+import Modal                        from 'react-modal';
+import slug                         from 'slug';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory }               from 'react-router';
+import { useRef, useState }         from 'react';
 
 import { AlertComponent } from './alert';
 import { ReactGA }        from '../utils/analytics';
 import { createDex }      from '../actions/dex';
 
-export class DexCreate extends Component {
+export function DexCreateComponent ({ isOpen, onRequestClose }) {
+  const dispatch = useDispatch();
 
-  constructor (props) {
-    super(props);
-    const { games } = props;
-    const latestGame = games[0];
+  const history = useHistory();
 
-    this.state = {
-      error: null,
-      url: null,
-      game: latestGame.id,
-      regional: !latestGame.game_family.national_support
-    };
-  }
+  const formRef = useRef(null);
 
-  onChange = (e) => {
-    const { gamesById } = this.props;
-    const game = e.target.value;
+  const games = useSelector(({ games }) => games);
+  const gamesById = useSelector(({ gamesById }) => gamesById);
+  const session = useSelector(({ session }) => session);
 
-    if (!gamesById[game].game_family.regional_support) {
-      this.setState({ regional: false });
+  const [error, setError] = useState(null);
+  const [title, setTitle] = useState('');
+  const [game, setGame] = useState(games[0].id);
+  const [regional, setRegional] = useState(!games[0].game_family.national_support);
+  const [shiny, setShiny] = useState(false);
+
+  const handleRequestClose = () => {
+    setError(null);
+    setTitle('');
+    setGame(games[0].id);
+    setRegional(!games[0].game_family.national_support);
+    setShiny(false);
+    onRequestClose();
+  };
+
+  const handleGameChange = (e) => {
+    const newGame = e.target.value;
+
+    if (!gamesById[newGame].game_family.regional_support) {
+      setRegional(false);
     }
 
-    if (!gamesById[game].game_family.national_support) {
-      this.setState({ regional: true });
+    if (!gamesById[newGame].game_family.national_support) {
+      setRegional(true);
     }
 
-    this.setState({ game });
-  }
+    setGame(newGame);
+  };
 
-  scrollToTop () {
-    if (this._form) {
-      this._form.scrollTop = 0;
-    }
-  }
-
-  onRequestClose = () => {
-    const { games } = this.props;
-    const latestGame = games[0];
-
-    this.setState({
-      error: null,
-      url: null,
-      game: latestGame.id,
-      regional: !latestGame.game_family.national_support
-    });
-    this.props.onRequestClose();
-  }
-
-  onSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { createDex, redirect, session } = this.props;
-    const { game, regional } = this.state;
-    const title = this._title.value;
-    const shiny = this._shiny.checked;
     const payload = {
       username: session.username,
       payload: { title, shiny, game, regional }
     };
 
-    this.setState({ ...this.state, error: null });
+    setError(null);
 
-    createDex(payload)
-    .then((dex) => {
+    try {
+      const dex = await dispatch(createDex(payload));
       ReactGA.event({ action: 'create', category: 'Dex' });
-
-      redirect(`/u/${session.username}/${dex.slug}`);
-    })
-    .catch((err) => {
-      this.setState({ ...this.state, error: err.message });
-      this.scrollToTop();
-    });
-  }
-
-  render () {
-    const { games, gamesById, isOpen, session } = this.props;
-    const { error, game, regional, url } = this.state;
-
-    if (!isOpen || !games) {
-      return null;
+      history.push(`/u/${session.username}/${dex.slug}`);
+    } catch (err) {
+      setError(err.message);
+      if (formRef.current) {
+        formRef.current.scrollTop = 0;
+      }
     }
+  };
 
-    return (
-      <Modal className="modal" overlayClassName="modal-overlay" isOpen={isOpen} onRequestClose={this.onRequestClose} contentLabel="Create a New Dex">
-        <div className="form" ref={(c) => this._form = c}>
-          <h1>Create New Dex</h1>
-          <form onSubmit={this.onSubmit} className="form-column">
-            <AlertComponent message={error} type="error" />
-            <div className="form-group">
-              <div className="form-note">/u/{session.username}/{slug(url || 'Living Dex', { lower: true })}</div>
-              <label htmlFor="dex_title">Title</label>
-              <input className="form-control" ref={(c) => this._title = c} name="dex_title" id="dex_title" type="text" maxLength="300" required placeholder="Living Dex" onChange={() => this.setState({ url: this._title.value })} />
-              <i className="fa fa-asterisk" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="game">Game</label>
-              <select className="form-control" onChange={this.onChange} value={game}>
-                {games.map((game) => <option key={game.id} value={game.id}>{game.name}</option>)}
-              </select>
-              <i className="fa fa-chevron-down" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="regional">Regionality</label>
-              <div className={`radio ${gamesById[game].game_family.national_support ? '' : 'disabled'}`}>
-                <label title={gamesById[game].game_family.national_support ? '' : 'National dex is not supported for this game at this time.'}>
-                  <input type="radio" name="regional" checked={!regional} disabled={!gamesById[game].game_family.national_support} value="national" onChange={() => this.setState({ regional: false })} />
-                  <span className="radio-custom"><span /></span>National
-                </label>
-              </div>
-              <div className={`radio ${gamesById[game].game_family.regional_support ? '' : 'disabled'}`}>
-                <label title={gamesById[game].game_family.regional_support ? '' : 'Regional dex is not supported for this game at this time.'}>
-                  <input type="radio" name="regional" checked={regional} disabled={!gamesById[game].game_family.regional_support} value="regional" onChange={() => this.setState({ regional: true })} />
-                  <span className="radio-custom"><span /></span>Regional
-                </label>
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="type">Type</label>
-              <div className="radio">
-                <label>
-                  <input type="radio" name="type" defaultChecked />
-                  <span className="radio-custom"><span /></span>Normal
-                </label>
-              </div>
-              <div className="radio">
-                <label>
-                  <input ref={(c) => this._shiny = c} type="radio" name="type" />
-                  <span className="radio-custom"><span /></span>Shiny
-                </label>
-              </div>
-            </div>
-            <button className="btn btn-blue" type="submit">Create <i className="fa fa-long-arrow-right" /></button>
-          </form>
-        </div>
-        <p><a className="link" onClick={this.onRequestClose}>Go Back</a></p>
-      </Modal>
-    );
+  const handleTitleChange = (e) => setTitle(e.target.value);
+
+  if (!isOpen || !games) {
+    return null;
   }
 
+  return (
+    <Modal
+      className="modal"
+      overlayClassName="modal-overlay"
+      isOpen={isOpen}
+      onRequestClose={handleRequestClose}
+      contentLabel="Create a New Dex"
+    >
+      <div className="form" ref={formRef}>
+        <h1>Create New Dex</h1>
+        <form onSubmit={handleSubmit} className="form-column">
+          <AlertComponent message={error} type="error" />
+          <div className="form-group">
+            <div className="form-note">/u/{session.username}/{slug(title || 'Living Dex', { lower: true })}</div>
+            <label htmlFor="dex_title">Title</label>
+            <input
+              className="form-control"
+              name="dex_title"
+              id="dex_title"
+              type="text"
+              maxLength="300"
+              required
+              placeholder="Living Dex"
+              onChange={handleTitleChange}
+              value={title}
+            />
+            <i className="fa fa-asterisk" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="game">Game</label>
+            <select
+              className="form-control"
+              onChange={handleGameChange}
+              value={game}
+            >
+              {games.map((game) => <option key={game.id} value={game.id}>{game.name}</option>)}
+            </select>
+            <i className="fa fa-chevron-down" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="regional">Regionality</label>
+            <div className={`radio ${gamesById[game].game_family.national_support ? '' : 'disabled'}`}>
+              <label title={gamesById[game].game_family.national_support ? '' : 'National dex is not supported for this game at this time.'}>
+                <input
+                  type="radio"
+                  name="regional"
+                  checked={!regional}
+                  disabled={!gamesById[game].game_family.national_support}
+                  value="national"
+                  onChange={() => setRegional(false)}
+                />
+                <span className="radio-custom"><span /></span>National
+              </label>
+            </div>
+            <div className={`radio ${gamesById[game].game_family.regional_support ? '' : 'disabled'}`}>
+              <label title={gamesById[game].game_family.regional_support ? '' : 'Regional dex is not supported for this game at this time.'}>
+                <input
+                  type="radio"
+                  name="regional"
+                  checked={regional}
+                  disabled={!gamesById[game].game_family.regional_support}
+                  value="regional"
+                  onChange={() => setRegional(true)}
+                />
+                <span className="radio-custom"><span /></span>Regional
+              </label>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="type">Type</label>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  name="type"
+                  onChange={() => setShiny(false)}
+                  checked={!shiny}
+                />
+                <span className="radio-custom"><span /></span>Normal
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  name="type"
+                  onChange={() => setShiny(true)}
+                  checked={shiny}
+                />
+                <span className="radio-custom"><span /></span>Shiny
+              </label>
+            </div>
+          </div>
+          <button className="btn btn-blue" type="submit">Create <i className="fa fa-long-arrow-right" /></button>
+        </form>
+      </div>
+      <p><a className="link" onClick={handleRequestClose}>Go Back</a></p>
+    </Modal>
+  );
 }
-
-function mapStateToProps ({ games, gamesById, session }) {
-  return { games, gamesById, session };
-}
-
-function mapDispatchToProps (dispatch) {
-  return {
-    createDex: (payload) => dispatch(createDex(payload)),
-    redirect: (path) => dispatch(push(path))
-  };
-}
-
-export const DexCreateComponent = connect(mapStateToProps, mapDispatchToProps)(DexCreate);
