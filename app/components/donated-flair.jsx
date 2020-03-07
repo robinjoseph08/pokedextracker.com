0fc5860882261035e7a901dbc1ca6a12080d3464
Robin Joseph <robinmjoseph08@gmail.com>
2020-03-07 10:08:39 -0800

    update donated flair component

--- app/components/donated-flair.jsx
+++ app/components/donated-flair.jsx
@@ -1,15 +1,11 @@
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-export function DonatedFlair ({ user }) {
+export function DonatedFlairComponent () {
+  const user = useSelector(({ currentUser, users }) => users[currentUser]);
+
   if (!user.donated) {
     return null;
   }
 
   return <img className="donated-flair emoji" title="This user has donated!" src="/emoji_star2.png" />;
 }
-
-function mapStateToProps ({ currentUser, users }) {
-  return { user: users[currentUser] };
-}
-
-export const DonatedFlairComponent = connect(mapStateToProps)(DonatedFlair);
