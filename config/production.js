14cc2c659e4cfedbb507f01ea4793cc7b9a5cb1d
Robin Joseph <robinmjoseph08@gmail.com>
2020-05-10 23:24:41 -0700

    feat(deploy): update for new cluster (#373)

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 export const Production = {
-  API_HOST: 'https://api.pokedextracker.com',
+  API_HOST: 'https://pokedextracker.com/api',
   ENVIRONMENT: 'production',
   GA_ID: 'UA-45307701-4',
   SENTRY_DSN: 'https://dc4726f104294d3ebe692a9df024dcb5@sentry.io/103004',
