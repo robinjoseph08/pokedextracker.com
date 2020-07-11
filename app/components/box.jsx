ee64675241307b0710775cd17a982a8111d2b1c3
Robin Joseph <robinmjoseph08@gmail.com>
2020-07-10 19:15:21 -0700

    feat(boxes): handle sword/shield expansion pass

--- app/components/box.jsx
+++ app/components/box.jsx
@@ -24,6 +24,22 @@ export function Box ({ captures, deferred }) {
     const firstNumber = dex.regional ? firstPokemon[`${dex.game.game_family.id}_id`] : firstPokemon.national_id;
     const lastNumber = dex.regional ? lastPokemon[`${dex.game.game_family.id}_id`] : lastPokemon.national_id;
     title = `${padding(firstNumber, 3)} - ${padding(lastNumber, 3)}`;
+  } else if (title.indexOf('reset') === 0) {
+    const parts = title.split(':');
+    const offset = parseInt(parts[1]);
+    const prefix = parts[2];
+
+    let firstNumber = dex.regional ? firstPokemon[`${dex.game.game_family.id}_id`] : firstPokemon.national_id;
+    let lastNumber = dex.regional ? lastPokemon[`${dex.game.game_family.id}_id`] : lastPokemon.national_id;
+
+    firstNumber -= offset;
+    lastNumber -= offset;
+
+    title = `${padding(firstNumber, 3)} - ${padding(lastNumber, 3)}`;
+
+    if (prefix) {
+      title = `${prefix} ${title}`;
+    }
   }
 
   if (!render) {
